c2e272606c6af8d34d13436b7c1ea930e50cc108
JorgeCruz19 <jorgecsibrian@gmail.com>
2022-08-18 13:56:05 -0600

    Add loading and error 404 image

--- js/index.js
+++ js/index.js
@@ -19,24 +19,29 @@ $btnToggle.addEventListener("click", function () {
 
 $formSearch.addEventListener("submit", async (e) => {
   e.preventDefault();
-  await fetchUser(e.target.elements.username.value);
+  await fetchUserAccount(e.target.elements.username.value);
 });
 
-const fetchUser = async (username = "octocat") => {
+const fetchUserAccount = async (username = "octocat") => {
   try {
+    loading();
     const res = await fetch(`${API_URL}/${username}`);
     if (!res.ok || res.status === 404) {
-      $card.innerHTML = `<p class="is-error">Username not found</p>`;
+      $card.innerHTML = `
+      <div class="is-centered">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="error-404" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 122.88 98.31" style="enable-background:new 0 0 122.88 98.31" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M8.32,0h106.24c4.57,0,8.32,3.74,8.32,8.32V90c0,4.57-3.74,8.32-8.32,8.32l-106.24,0C3.74,98.31,0,94.57,0,90 V8.32C0,3.74,3.74,0,8.32,0L8.32,0z M37.46,65.4H23.01v-6.53l14.45-17.18h6.9v17.56h3.59v6.15h-3.59v5.35h-6.9V65.4L37.46,65.4z M37.46,59.25v-9.02l-7.63,9.02H37.46L37.46,59.25z M49.77,56.34c0-5.43,0.98-9.23,2.93-11.39c1.96-2.17,4.94-3.25,8.93-3.25 c1.92,0,3.51,0.24,4.74,0.71c1.24,0.47,2.24,1.09,3.02,1.85c0.78,0.76,1.4,1.56,1.84,2.39c0.45,0.84,0.81,1.82,1.08,2.93 c0.53,2.13,0.8,4.35,0.8,6.67c0,5.18-0.88,8.97-2.63,11.37c-1.75,2.4-4.77,3.61-9.06,3.61c-2.4,0-4.34-0.39-5.83-1.15 c-1.48-0.77-2.69-1.89-3.65-3.37c-0.69-1.05-1.22-2.49-1.61-4.31C49.96,60.56,49.77,58.55,49.77,56.34L49.77,56.34z M57.63,56.35 c0,3.63,0.32,6.12,0.96,7.45c0.64,1.33,1.58,2,2.8,2c0.8,0,1.5-0.28,2.1-0.84c0.59-0.57,1.02-1.46,1.3-2.68 c0.28-1.22,0.42-3.13,0.42-5.71c0-3.79-0.32-6.34-0.96-7.65c-0.65-1.3-1.61-1.96-2.89-1.96c-1.32,0-2.26,0.67-2.85,2 C57.92,50.28,57.63,52.75,57.63,56.35L57.63,56.35z M89.37,65.4H74.92v-6.53l14.45-17.18h6.9v17.56h3.59v6.15h-3.59v5.35h-6.9V65.4 L89.37,65.4z M89.37,59.25v-9.02l-7.63,9.02H89.37L89.37,59.25z M117.97,23.29H5.29v67.51c0,0.64,0.25,1.2,0.67,1.63 c0.42,0.43,0.99,0.67,1.63,0.67h108.04c0.64,0,1.2-0.25,1.63-0.67c0.43-0.43,0.67-0.99,0.67-1.63l0-67.51H117.97L117.97,23.29z M106.64,9.35c2.27,0,4.11,1.84,4.11,4.11c0,2.27-1.84,4.11-4.11,4.11c-2.27,0-4.11-1.84-4.11-4.11 C102.54,11.19,104.38,9.35,106.64,9.35L106.64,9.35z M78.8,9.35c2.27,0,4.11,1.84,4.11,4.11c0,2.27-1.84,4.11-4.11,4.11 c-2.27,0-4.11-1.84-4.11-4.11C74.69,11.19,76.53,9.35,78.8,9.35L78.8,9.35z M92.72,9.35c2.27,0,4.11,1.84,4.11,4.11 c0,2.27-1.84,4.11-4.11,4.11c-2.27,0-4.11-1.84-4.11-4.11C88.61,11.19,90.45,9.35,92.72,9.35L92.72,9.35z"/></g></svg>
+        <p class="is-error">'User account not found'</p>
+      </div>`;
       return;
     }
     const data = await res.json();
-    loadTemplate(data);
+    loadUserTemplate(data);
   } catch (error) {
     console.error(error);
   }
 };
 
-const loadTemplate = (data) => {
+const loadUserTemplate = (data) => {
   const { name, avatar_url, followers, following, public_repos, created_at, twitter_username, bio, location, company, html_url, login, blog } = data;
   const templateHTML = `
           <div class="card-header">
@@ -122,6 +127,48 @@ const loadTemplate = (data) => {
   $card.innerHTML = templateHTML;
 };
 
+const loading = () => {
+  const loadingTemplateHTML = `
+    <div class="is-centered">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        style="background: none; shape-rendering: auto"
+        width="150px"
+        height="150px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <g transform="translate(50 50)">
+          <g transform="scale(0.8)">
+            <g transform="translate(-50 -50)">
+              <g>
+                <animateTransform
+                  attributeName="transform"
+                  type="translate"
+                  repeatCount="indefinite"
+                  dur="1.1363636363636365s"
+                  values="-20 -20;20 -20;0 20;-20 -20"
+                  keyTimes="0;0.33;0.66;1"
+                />
+                <path
+                  fill="#ffffff"
+                  d="M44.19 26.158c-4.817 0-9.345 1.876-12.751 5.282c-3.406 3.406-5.282 7.934-5.282 12.751 c0 4.817 1.876 9.345 5.282 12.751c3.406 3.406 7.934 5.282 12.751 5.282s9.345-1.876 12.751-5.282 c3.406-3.406 5.282-7.934 5.282-12.751c0-4.817-1.876-9.345-5.282-12.751C53.536 28.033 49.007 26.158 44.19 26.158z"
+                />
+                <path
+                  fill="#141c2f"
+                  d="M78.712 72.492L67.593 61.373l-3.475-3.475c1.621-2.352 2.779-4.926 3.475-7.596c1.044-4.008 1.044-8.23 0-12.238 c-1.048-4.022-3.146-7.827-6.297-10.979C56.572 22.362 50.381 20 44.19 20C38 20 31.809 22.362 27.085 27.085 c-9.447 9.447-9.447 24.763 0 34.21C31.809 66.019 38 68.381 44.19 68.381c4.798 0 9.593-1.425 13.708-4.262l9.695 9.695 l4.899 4.899C73.351 79.571 74.476 80 75.602 80s2.251-0.429 3.11-1.288C80.429 76.994 80.429 74.209 78.712 72.492z M56.942 56.942 c-3.406 3.406-7.934 5.282-12.751 5.282s-9.345-1.876-12.751-5.282c-3.406-3.406-5.282-7.934-5.282-12.751 c0-4.817 1.876-9.345 5.282-12.751c3.406-3.406 7.934-5.282 12.751-5.282c4.817 0 9.345 1.876 12.751 5.282 c3.406 3.406 5.282 7.934 5.282 12.751C62.223 49.007 60.347 53.536 56.942 56.942z"
+                />
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+      <p>Loading...</p>
+    </div>`;
+  $card.innerHTML = loadingTemplateHTML;
+};
+
 document.addEventListener("DOMContentLoaded", async () => {
-  await fetchUser();
+  await fetchUserAccount();
 });
